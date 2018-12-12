3a1d2a342825a03ad74cd30252657c07ca73f0fc
coas5 <coas5@wireless-staff-pt3-117-193.lut.ac.uk>
2018-12-12 16:38:58 +0000

    introducing PPO agent

--- tmod-direct.py
+++ tmod-direct.py
@@ -32,25 +32,6 @@ def dqn_pixel_atari(name):
     config.double_q = False
     run_episodes(DQNAgent(config))
 
-def dqn_pixel_atari_forBeam(name):
-    config = Config()
-    config.history_length = 4
-    config.task_fn = lambda: PixelAtari(name, history_length=config.history_length,
-                                        log_dir=get_default_log_dir(dqn_pixel_atari.__name__))
-    config.optimizer_fn = lambda params: torch.optim.RMSprop(params, lr=0.00025, alpha=0.95, eps=0.01)
-    # config.network_fn = lambda state_dim, action_dim: VanillaNet(action_dim, NatureConvBody())
-    config.network_fn = lambda state_dim, action_dim: DuelingNet(action_dim, NatureConvBody())
-    config.policy_fn = lambda: GreedyPolicy(LinearSchedule(1.0, 0.1, 1e6))
-    config.replay_fn = lambda: Replay(memory_size=int(1e6), batch_size=32)
-    config.state_normalizer = ImageNormalizer()
-    config.reward_normalizer = SignNormalizer()
-    config.discount = 0.99
-    config.target_network_update_freq = 10000
-    config.exploration_steps= 50000
-    config.logger = get_logger()
-    # config.double_q = True
-    config.double_q = False
-    run_episodes(DQNAgent(config))
 # L2M
 def mod_dqn_pixel_atari_2l(name):
     config = Config()
