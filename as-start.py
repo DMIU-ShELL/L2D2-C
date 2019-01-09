4cbfc05c907c88516d69ecafca120c4f03251758
coas5 <coas5@wireless-staff-pt3-112-178.lut.ac.uk>
2019-01-09 11:45:51 +0000

    line change suggested by Pawel

--- as-start.py
+++ as-start.py
@@ -299,7 +299,8 @@ def quantile_regression_dqn_pixel_atari(name):
     config.log_dir = get_default_log_dir(config.expType) + config.expID
 #    config.max_steps = 5 * 1000000
     config.episode_limit = 100000
-    config.save_interval = 20
+    config.save_interval = 100
+    config.log_modulation = 0
 
     config.history_length = 4
     config.task_fn = lambda: PixelAtari(name, frame_skip=4, history_length=config.history_length,
@@ -329,7 +330,7 @@ def quantile_regression_dqn_pixel_atari_mod(name):
 #    config.max_steps = 5 * 1000000
     config.episode_limit = 120000
     config.save_interval = 100
-    config.log_modulation = 1
+    config.log_modulation = 0
 
     config.history_length = 4
     config.task_fn = lambda: PixelAtari(name, frame_skip=4, history_length=config.history_length,
@@ -348,6 +349,37 @@ def quantile_regression_dqn_pixel_atari_mod(name):
     config.double_q = False
     config.num_quantiles = 200
     run_episodes(QuantileRegressionDQNAgent_mod(config))
+
+def quantile_regression_dqn_pixel_atari_mod_surprise(name):
+    '''first version of the QR with modulation, using the relu6s05p05'''
+    config = Config()
+    config.seed = 1
+    config.expType = "qrdqn_pa_" + name
+    config.expID = "mod_surp"
+    config.log_dir = get_default_log_dir(config.expType) + config.expID
+#    config.max_steps = 5 * 1000000
+    config.episode_limit = 120000
+    config.save_interval = 100
+    config.log_modulation = 1
+
+    config.history_length = 4
+    config.task_fn = lambda: PixelAtari(name, frame_skip=4, history_length=config.history_length,
+                                        log_dir=config.log_dir)
+    config.optimizer_fn = lambda params: torch.optim.Adam(params, lr=0.00005, eps=0.01 / 32)
+    config.network_fn = lambda state_dim, action_dim: \
+        QuantileNetMod(action_dim, config.num_quantiles, Mod3LNatureConvBody_direct_relu6_shift05p05())
+    config.policy_fn = lambda: GreedyPolicy(LinearSchedule(1.0, 0.01, 1e6))
+    config.replay_fn = lambda: Replay(memory_size=int(1e6), batch_size=32)
+    config.state_normalizer = ImageNormalizer()
+    config.reward_normalizer = SignNormalizer()
+    config.discount = 0.99
+    config.target_network_update_freq = 10000
+    config.exploration_steps= 50000
+    config.logger = get_logger(log_dir=config.log_dir)
+    config.double_q = False
+    config.num_quantiles = 200
+    run_episodes(QuantileRegressionDQNAgent_mod_surp(config))
+
 
 def quantile_regression_dqn_pixel_atari_mod_noframeskip(name):
     '''first version of the QR with modulation, using the relu6s05p05'''
@@ -487,6 +519,32 @@ def ppo_pa_mod(name):
     config.iteration_log_interval = 1
     run_iterations(PPOAgent_L2M_Mem(config))
 
+def ddpg_pixel():
+    config = Config()
+    log_dir = get_default_log_dir(ddpg_pixel.__name__)
+    config.task_fn = lambda **kwargs: PixelBullet('AntBulletEnv-v0', frame_skip=1,
+                                           history_length=4, **kwargs)
+    config.evaluation_env = config.task_fn(log_dir=log_dir)
+
+    phi_body=DDPGConvBody()
+    config.network_fn = lambda state_dim, action_dim: DeterministicActorCriticNet(
+        state_dim, action_dim, phi_body=phi_body,
+        actor_body=FCBody(phi_body.feature_dim, (50, ), gate=F.tanh),
+        critic_body=OneLayerFCBodyWithAction(phi_body.feature_dim, action_dim, 50, gate=F.tanh),
+        actor_opt_fn=lambda params: torch.optim.Adam(params, lr=1e-4),
+        critic_opt_fn=lambda params: torch.optim.Adam(params, lr=1e-3))
+
+    config.replay_fn = lambda: Replay(memory_size=1000000, batch_size=16)
+    config.discount = 0.99
+    config.state_normalizer = ImageNormalizer()
+    config.max_steps = 1e7
+    config.random_process_fn = lambda action_dim: OrnsteinUhlenbeckProcess(
+        size=(action_dim, ), std=LinearSchedule(0.2))
+    config.min_memory_size = 64
+    config.target_network_mix = 1e-3
+    config.logger = get_logger(file_name=ddpg_pixel.__name__)
+    run_episodes(DDPGAgent(config))
+
 if __name__ == '__main__':
     mkdir('data/video')
     mkdir('dataset')
@@ -510,7 +568,10 @@ if __name__ == '__main__':
     #mod_dqn_pixel_atari_3l_diff('BreakoutNoFrameskip-v4')
 
     #quantile_regression_dqn_pixel_atari('BreakoutNoFrameskip-v4')
+#    ddpg_pixel()
     quantile_regression_dqn_pixel_atari_mod('BreakoutNoFrameskip-v4')
+#    quantile_regression_dqn_pixel_atari_mod_surprise('BreakoutNoFrameskip-v4')
+
 #    quantile_regression_dqn_pixel_atari_mod_noframeskip('Riverraid-v4')
     #quantile_regression_dqn_pixel_atari_noframeskip('Riverraid-v4')
 
