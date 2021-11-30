72dfc47c4c112459241116d6cc7babd0a756ed41
Ese <17943860+dlpbc@users.noreply.github.com>
2021-11-30 14:49:04 +0000

    initial commit of the modified version of the repo (https://github.com/shangtongzhang/deeprl). the code is setup to include multi-task lifelong (continual learning) with synaptic consolidation, and also the default single task learning in deeprl

---
+++ deep_rl/agent/BaseAgent.py
@@ -0,0 +1,131 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import torch
+import numpy as np
+from ..utils import *
+
+class BaseAgent:
+    def __init__(self, config):
+        self.config = config
+        self.evaluation_env = self.config.evaluation_env
+        if self.evaluation_env is not None:
+            self.evaluation_state = self.evaluation_env.reset()
+            self.evaluation_return = 0
+
+    def close(self):
+        if hasattr(self.task, 'close'):
+            self.task.close()
+        if hasattr(self.evaluation_env, 'close'):
+            self.evaluation_env.close()
+
+    def save(self, filename):
+        torch.save(self.network.state_dict(), filename)
+
+    def load(self, filename):
+        state_dict = torch.load(filename, map_location=lambda storage, loc: storage)
+        self.network.load_state_dict(state_dict)
+
+    def evaluation_action(self, state):
+        self.config.state_normalizer.set_read_only()
+        state = self.config.state_normalizer(np.stack([state]))
+        action = self.network.predict(state, to_numpy=True)
+        self.config.state_normalizer.unset_read_only()
+        return np.argmax(action.flatten())
+
+    def deterministic_episode(self):
+        env = self.config.evaluation_env
+        state = env.reset()
+        total_rewards = 0
+        while True:
+            action = self.evaluation_action(state)
+            state, reward, done, _ = env.step(action)
+            total_rewards += reward
+            if done:
+                break
+        return total_rewards
+
+    def evaluation_episodes(self):
+        interval = self.config.evaluation_episodes_interval
+        if not interval or self.total_steps % interval:
+            return
+        rewards = []
+        for ep in range(self.config.evaluation_episodes):
+            rewards.append(self.deterministic_episode())
+        self.config.logger.info('evaluation episode return: %f(%f)' % (
+            np.mean(rewards), np.std(rewards) / np.sqrt(len(rewards))))
+
+    def evaluate(self, steps=1):
+        config = self.config
+        if config.evaluation_env is None or self.config.evaluation_episodes_interval:
+            return
+        for _ in range(steps):
+            action = self.evaluation_action(self.evaluation_state)
+            self.evaluation_state, reward, done, _ = self.evaluation_env.step(action)
+            self.evaluation_return += reward
+            if done:
+                self.evaluation_state = self.evaluation_env.reset()
+                self.config.logger.info('evaluation episode return: %f' % (self.evaluation_return))
+                self.evaluation_return = 0
+
+class BaseContinualLearnerAgent(BaseAgent):
+    def __init__(self, config):
+        BaseAgent.__init__(self, config)
+
+    def consolidate(self, config=None):
+        raise NotImplementedError
+
+    def penalty(self):
+        raise NotImplementedError
+
+    def evaluation_action(self, state, task_label):
+        self.config.state_normalizer.set_read_only()
+        state = self.config.state_normalizer(np.stack([state]))
+        task_label = np.stack([task_label])
+        out = self.network.predict(state, task_label=task_label)
+        self.config.state_normalizer.unset_read_only()
+        if isinstance(out, dict) or isinstance(out, list) or isinstance(out, tuple):
+            # for actor-critic and policy gradient approaches
+            logits = out[0]
+            action = np.argmax(logits.cpu().numpy().flatten())
+            ret = {'logits': out[0], 'sampled_action': out[1], 'log_prob': out[2], 
+                'entropy': out[3], 'value': out[4], 'deterministic_action': action}
+            return action, ret
+        else:
+            # for dqn approaches
+            q = out
+            q = out.detach().cpu().numpy().ravel()
+            return np.argmax(q), {'logits': q}
+
+    def deterministic_episode(self):
+        epi_info = {'logits': [], 'sampled_action': [], 'log_prob': [], 'entropy': [],
+            'value': [], 'deterministic_action': [], 'reward': [], 'terminal': []}
+
+        #env = self.config.evaluation_env
+        env = self.evaluation_env
+        state = env.reset()
+        task_label = env.get_task()['task_label']
+        total_rewards = 0
+        while True:
+            action, output_info = self.evaluation_action(state, task_label)
+            state, reward, done, _ = env.step(action)
+            total_rewards += reward
+            for k, v in output_info.items(): epi_info[k].append(v)
+            epi_info['reward'].append(reward)
+            epi_info['terminal'].append(done)
+            if done: break
+        return total_rewards, epi_info
+
+    def evaluate_cl(self, num_iterations=100):
+        # evaluation method for continual learning agents
+        rewards = []
+        episodes = []
+        with torch.no_grad():
+            for ep in range(num_iterations):
+                total_episode_reward, episode_info = self.deterministic_episode()
+                rewards.append(total_episode_reward)
+                episodes.append(episode_info)
+        return rewards, episodes
---
+++ deep_rl/component/task.py
@@ -0,0 +1,464 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+import copy
+from .atari_wrapper import *
+import multiprocessing as mp
+import sys
+from .bench import Monitor
+from ..utils import *
+import uuid
+
+class BaseTask:
+    def __init__(self):
+        pass
+
+    def set_monitor(self, env, log_dir):
+        if log_dir is None:
+            return env
+        mkdir(log_dir)
+        return Monitor(env, '%s/%s' % (log_dir, uuid.uuid4()))
+
+    def reset(self):
+        #print 'base task reset called'
+        return self.env.reset()
+
+    def step(self, action):
+        next_state, reward, done, info = self.env.step(action)
+        #print 'base task step called'
+        #print self.env
+        #print done
+        if done:
+            next_state = self.env.reset()
+        return next_state, reward, done, info
+
+    def seed(self, random_seed):
+        return self.env.seed(random_seed)
+
+class ClassicalControl(BaseTask):
+    def __init__(self, name, max_steps=200, log_dir=None):
+        # Removed from name = "Cart_Pole-v0"
+        BaseTask.__init__(self)
+        self.name = name
+        self.env = gym.make(self.name)
+        self.env._max_episode_steps = max_steps
+        self.action_dim = self.env.action_space.n
+        self.state_dim = self.env.observation_space.shape[0]
+        self.env = self.set_monitor(self.env, log_dir)
+
+class DynamicGrid(BaseTask):
+    def __init__(self, name, max_steps=200, log_dir=None):
+        BaseTask.__init__(self)
+        self.name = name
+        import dynamic_grid
+        self.env = gym.make(self.name)
+        self.env._max_episode_steps = max_steps
+        self.action_dim = self.env.action_space.n
+        self.state_dim = self.env.observation_space.shape
+        self.env = self.set_monitor(self.env, log_dir)
+
+        # total number of unique tasks in this class instance. note, the actual
+        # environment (wrapped by this class) has many more task variations
+        num_tasks = 3
+        change_matrix = np.eye(num_tasks).astype(np.bool)
+        self.tasks = self.env.unwrapped.unwrapped.random_tasks(change_matrix)
+        self.current_task = self.tasks[0]
+
+        self.task_label_dim = 64
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if done: state = self.reset()
+        return state, reward, done, info
+
+    def reset(self):
+        state = self.env.reset()
+        return state
+
+    def reset_task(self, task_info):
+        self.set_task(task_info)
+        return self.reset()
+
+    def set_task(self, task_info):
+        msg = '`{0}` parameter should be included in `task_info` in the DynamicGrid env'
+        assert 'goal_location' in task_info.keys(), msg.format('goal_location')
+        assert 'transition_dynamics' in task_info.keys(), msg.format('transition_dynamics')
+        assert 'permute_input' in task_info.keys(), msg.format('permute_input')
+        self.env.unwrapped.unwrapped.set_task(task_info)
+        self.current_task = task_info
+
+    def get_task(self):
+        return self.current_task
+
+    def get_all_tasks(self, requires_task_label=True):
+        if requires_task_label:
+            tasks_label=np.random.uniform(low=-1.,high=1.,size=(len(self.tasks),self.task_label_dim))
+            tasks = copy.deepcopy(self.tasks)
+            for task, label in zip(tasks, tasks_label):
+                task['task_label'] = label
+            return tasks
+        else:
+            return self.tasks
+    
+    def random_tasks(self, num_tasks, requires_task_label=True):
+        tasks_idx = np.random.randint(low=0, high=len(self.tasks), size=(num_tasks,))
+        if requires_task_label:
+            all_tasks = copy.deepcopy(self.tasks)
+            tasks_label=np.random.uniform(low=-1.,high=1.,size=(len(self.tasks),self.task_label_dim))
+            tasks = []
+            for idx in tasks_idx:
+                task = all_tasks[idx]
+                task['task_label'] = tasks_label[idx]
+                tasks.append(task)
+            return tasks
+        else:
+            tasks = [self.tasks[idx] for idx in tasks_idx]
+            return tasks
+
+class DynamicGridFlatObs(DynamicGrid):
+    # Dynamic Grid environment with flattend (1d vector) observations.
+	# 2D images are flattened into 1D vectors
+    def __init__(self, name, max_steps=200, log_dir=None):
+        super(DynamicGridFlatObs, self).__init__(name, max_steps, log_dir)
+        self.state_dim = int(np.prod(self.env.observation_space.shape))
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if done: state = self.reset()
+        return state.ravel(), reward, done, info
+
+    def reset(self):
+        state = self.env.reset()
+        return state.ravel()
+
+
+class CTgraph(BaseTask):
+    def __init__(self, name, env_config_path, log_dir=None):
+        BaseTask.__init__(self)
+        self.name = name
+        from gym_CTgraph import CTgraph_env
+        from gym_CTgraph.CTgraph_conf import CTgraph_conf
+        from gym_CTgraph.CTgraph_images import CTgraph_images
+        env = gym.make(name)
+        env_config = CTgraph_conf(env_config_path)
+        env_config = env_config.getParameters()
+        imageDataset = CTgraph_images(env_config)
+        self.env_config=env_config
+
+        state, _, _, _ = env.init(env_config, imageDataset)
+        env.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=state.shape)
+        self.action_dim = env.action_space.n
+        if env_config['image_dataset']['1D']:
+            self.state_dim = int(np.prod(env.observation_space.shape))
+        else:
+            self.state_dim = env.observation_space.shape
+
+        self.task_label_dim = 64
+        self.env = self.set_monitor(env, log_dir)
+
+        # get all tasks in graph environment instance
+        from itertools import product
+        depth = env_config['graph_shape']['depth']
+        branch = env_config['graph_shape']['depth']
+        tasks = list(product(list(range(branch)), repeat=depth))
+        self.tasks = [{'goal': np.array(task), 'task_label': None} for task in tasks]
+
+        #self.current_task = self.tasks[0]
+        self.current_task = None
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if done: state = self.reset()
+        if self.env_config['image_dataset']['1D']: state = state.ravel()
+        return state, reward, done, info
+
+    def reset(self):
+        state, _, _, _ = self.env.reset() # ctgraph returns state, reward, done, info in reset
+        if self.env_config['image_dataset']['1D']: state = state.ravel()
+        # return only state when reset is called to conform with other env in the repo
+        return state
+
+    def reset_task(self, taskinfo):
+        self.set_task(taskinfo)
+        return self.reset()
+
+    def set_task(self, taskinfo):
+        self.env.unwrapped.set_high_reward_path(taskinfo['goal'])
+        self.current_task = taskinfo
+    
+    def get_task(self):
+        return self.current_task
+
+    def get_all_tasks(self, requires_task_label=False):
+        if requires_task_label:
+            tasks_label=np.random.uniform(low=-1.,high=1.,size=(len(self.tasks),self.task_label_dim))
+            tasks = copy.deepcopy(self.tasks)
+            for task, label in zip(tasks, tasks_label):
+                task['task_label'] = label
+            return tasks
+        else:
+            return self.tasks
+    
+    def random_tasks(self, num_tasks, requires_task_label=True):
+        tasks_idx = np.random.randint(low=0, high=len(self.tasks), size=(num_tasks,))
+        if requires_task_label:
+            all_tasks = copy.deepcopy(self.tasks)
+            tasks_label =np.random.uniform(low=-1.,high=1.,size=(len(all_tasks),self.task_label_dim))
+            tasks_label = tasks_label.astype(np.float32)
+            tasks = []
+            for idx in tasks_idx:
+                task = all_tasks[idx]
+                task['task_label'] = tasks_label[idx]
+                tasks.append(task)
+            return tasks
+        else:
+            tasks = [self.tasks[idx] for idx in tasks_idx]
+            return tasks
+
+class CTgraphFlatObs(CTgraph):
+    # CTgraph environment with flattend (1d vector) observations.
+    # observations are flattenend whether 1D or 2D observations.
+    def __init__(self, name, env_config_path, log_dir=None):
+        super(CTgraphFlatObs, self).__init__(name, env_config_path, log_dir)
+        # overwrite previous written statedim to be flat 1d vector observations
+        self.state_dim = int(np.prod(self.env.observation_space.shape))
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if done: state = self.reset()
+        return state.ravel(), reward, done, info
+
+    def reset(self):
+        state, _, _, _ = self.env.reset() # ctgraph returns state, reward, done, info in reset
+        return state.ravel()
+
+
+class PixelAtari(BaseTask):
+    def __init__(self, name, seed=0, log_dir=None,
+                 frame_skip=4, history_length=4, dataset=False):
+        BaseTask.__init__(self)
+        env = make_atari(name, frame_skip)
+        env.seed(seed)
+        if dataset:
+            env = DatasetEnv(env)
+            self.dataset_env = env
+        env = self.set_monitor(env, log_dir)
+        env = wrap_deepmind(env, history_length=history_length)
+        self.env = env
+        self.action_dim = self.env.action_space.n
+        self.state_dim = self.env.observation_space.shape
+        self.name = name
+
+class RamAtari(BaseTask):
+    def __init__(self, name, no_op, frame_skip, log_dir=None):
+        BaseTask.__init__(self)
+        self.name = name
+        env = gym.make(name)
+        assert 'NoFrameskip' in env.spec.id
+        env = self.set_monitor(env, log_dir)
+        env = EpisodicLifeEnv(env)
+        env = NoopResetEnv(env, noop_max=no_op)
+        env = SkipEnv(env, skip=frame_skip)
+        if 'FIRE' in env.unwrapped.get_action_meanings():
+            env = FireResetEnv(env)
+        self.env = env
+        self.action_dim = self.env.action_space.n
+        self.state_dim = 128
+
+class Pendulum(BaseTask):
+    def __init__(self, log_dir=None):
+        BaseTask.__init__(self)
+        self.name = 'Pendulum-v0'
+        self.env = gym.make(self.name)
+        self.action_dim = self.env.action_space.shape[0]
+        self.state_dim = self.env.observation_space.shape[0]
+        self.env = self.set_monitor(self.env, log_dir)
+
+    def step(self, action):
+        return BaseTask.step(self, np.clip(2 * action, -2, 2))
+
+class Box2DContinuous(BaseTask):
+    def __init__(self, name, log_dir=None):
+        BaseTask.__init__(self)
+        self.name = name
+        self.env = gym.make(self.name)
+        self.action_dim = self.env.action_space.shape[0]
+        self.state_dim = self.env.observation_space.shape[0]
+        self.env = self.set_monitor(self.env, log_dir)
+
+    def step(self, action):
+        return BaseTask.step(self, np.clip(action, -1, 1))
+
+class Roboschool(BaseTask):
+    def __init__(self, name, log_dir=None):
+        import roboschool
+        BaseTask.__init__(self)
+        self.name = name
+        self.env = gym.make(self.name)
+        self.action_dim = self.env.action_space.shape[0]
+        self.state_dim = self.env.observation_space.shape[0]
+        self.env = self.set_monitor(self.env, log_dir)
+
+    def step(self, action):
+        return BaseTask.step(self, np.clip(action, -1, 1))
+
+class Bullet(BaseTask):
+    def __init__(self, name, log_dir=None):
+        import pybullet_envs
+        BaseTask.__init__(self)
+        self.name = name
+        self.env = gym.make(name)
+        self.action_dim = self.env.action_space.shape[0]
+        self.state_dim = self.env.observation_space.shape[0]
+        self.env = self.set_monitor(self.env, log_dir)
+
+    def step(self, action):
+        return BaseTask.step(self, np.clip(action, -1, 1))
+
+class PixelBullet(BaseTask):
+    def __init__(self, name, seed=0, log_dir=None, frame_skip=4, history_length=4):
+        import pybullet_envs
+        self.name = name
+        env = gym.make(name)
+        env.seed(seed)
+        env = RenderEnv(env)
+        env = self.set_monitor(env, log_dir)
+        env = SkipEnv(env, skip=frame_skip)
+        env = WarpFrame(env)
+        env = WrapPyTorch(env)
+        if history_length:
+            env = StackFrame(env, history_length)
+        self.action_dim = env.action_space.shape[0]
+        self.state_dim = env.observation_space.shape
+        self.env = env
+
+class ProcessTask:
+    def __init__(self, task_fn, log_dir=None):
+        self.pipe, worker_pipe = mp.Pipe()
+        self.worker = ProcessWrapper(worker_pipe, task_fn, log_dir)
+        self.worker.start()
+        self.pipe.send([ProcessWrapper.SPECS, None])
+        self.state_dim, self.action_dim, self.name = self.pipe.recv()
+
+    def step(self, action):
+        self.pipe.send([ProcessWrapper.STEP, action])
+        return self.pipe.recv()
+
+    def reset(self):
+        self.pipe.send([ProcessWrapper.RESET, None])
+        return self.pipe.recv()
+
+    def close(self):
+        self.pipe.send([ProcessWrapper.EXIT, None])
+
+    def reset_task(self, task_info):
+        self.pipe.send([ProcessWrapper.RESET_TASK, task_info])
+        return self.pipe.recv()
+
+    def set_task(self, task_info):
+        self.pipe.send([ProcessWrapper.SET_TASK, task_info])
+
+    def get_task(self):
+        self.pipe.send([ProcessWrapper.GET_TASK, None])
+        return self.pipe.recv()
+
+    def get_all_tasks(self, requires_task_label):
+        self.pipe.send([ProcessWrapper.GET_ALL_TASKS, requires_task_label])
+        return self.pipe.recv()
+    
+    def random_tasks(self, num_tasks, requires_task_label):
+        self.pipe.send([ProcessWrapper.RANDOM_TASKS, [num_tasks, requires_task_label]])
+        return self.pipe.recv()
+
+class ProcessWrapper(mp.Process):
+    STEP = 0
+    RESET = 1
+    EXIT = 2
+    SPECS = 3
+    RESET_TASK = 4
+    SET_TASK = 5
+    GET_TASK = 6
+    GET_ALL_TASKS = 7
+    RANDOM_TASKS = 8
+    def __init__(self, pipe, task_fn, log_dir):
+        mp.Process.__init__(self)
+        self.pipe = pipe
+        self.task_fn = task_fn
+        self.log_dir = log_dir
+
+    def run(self):
+        np.random.seed()
+        seed = np.random.randint(0, sys.maxsize)
+        task = self.task_fn(log_dir=self.log_dir)
+        task.seed(seed)
+        while True:
+            op, data = self.pipe.recv()
+            if op == self.STEP:
+                self.pipe.send(task.step(data))
+            elif op == self.RESET:
+                self.pipe.send(task.reset())
+            elif op == self.EXIT:
+                self.pipe.close()
+                return
+            elif op == self.SPECS:
+                self.pipe.send([task.state_dim, task.action_dim, task.name])
+            elif op == self.RESET_TASK:
+                self.pipe.send(task.reset_task(data))
+            elif op == self.SET_TASK:
+                self.pipe.send(task.set_task(data))
+            elif op == self.GET_TASK:
+                self.pipe.send(task.get_task())
+            elif op == self.GET_ALL_TASKS:
+                self.pipe.send(task.get_all_tasks(data))
+            elif op == self.RANDOM_TASKS:
+                self.pipe.send(task.random_tasks(*data))
+            else:
+                raise Exception('Unknown command')
+
+class ParallelizedTask:
+    def __init__(self, task_fn, num_workers, log_dir=None, single_process=False):
+
+        if single_process:
+            self.tasks = [task_fn(log_dir=log_dir) for _ in range(num_workers)]
+        else:
+            self.tasks = [ProcessTask(task_fn, log_dir) for _ in range(num_workers)]
+        self.state_dim = self.tasks[0].state_dim
+        self.action_dim = self.tasks[0].action_dim
+        self.name = self.tasks[0].name
+        self.single_process = single_process
+
+    def step(self, actions):
+        results = [task.step(action) for task, action in zip(self.tasks, actions)]
+        results = map(lambda x: np.stack(x), zip(*results))
+        return results
+
+    def reset(self):
+        results = [task.reset() for task in self.tasks]
+        return np.stack(results)
+
+    def close(self):
+        if self.single_process:
+            return
+        for task in self.tasks: task.close()
+
+    def reset_task(self, task_info):
+        results = [task.reset_task(task_info) for task in self.tasks]
+        return np.stack(results)
+
+    def set_task(self, task_info):
+        for task in self.tasks:
+            task.set_task(task_info)
+
+    def get_task(self, all_workers=False):
+        if not all_workers:
+            return self.tasks[0].get_task()
+        else:
+            return [task.get_task() for task in self.tasks]
+
+    def get_all_tasks(self, requires_task_label):
+        return self.tasks[0].get_all_tasks(requires_task_label)
+    
+    def random_tasks(self, num_tasks, requires_task_label):
+        return self.tasks[0].random_tasks(num_tasks, requires_task_label)
