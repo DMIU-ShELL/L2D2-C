85cccc6969a2f87adc7bbf746bf32e87c0c86508
coeb4 <e.ben-iwhiwhu@lboro.ac.uk>
2022-07-21 23:29:14 +0100

    *continualworld integration* added support for continalworld environment in the repository In deep_rl/component/task.py In run_ll.py In env_configs/continualworld_3.json
    *continuous action space*
    fixed bug to enable evaluation of policies in continuous action space
    environment (as continualworld is a continuous action space env).
    In deep_rl/component/BaseAgent.py
    
    *gaussian policy bug fix*
    bug fix in the continual learner gaussian policy.
    In deep_rl/network/netwrok_heads.py
    
     On branch shell
     Your branch is up to date with 'origin/shell'.
    
     Changes to be committed:
    	modified:   deep_rl/agent/BaseAgent.py
    	modified:   deep_rl/component/task.py
    	modified:   deep_rl/network/network_heads.py
    	new file:   env_configs/continualworld_3.json
    	modified:   run_ll.py

--- run_ll.py
+++ run_ll.py
@@ -317,6 +317,80 @@ def ppo_ll_minigrid(name, args):
                 dict_config[k] = str(dict_config[k])
         json.dump(dict_config, f)
 
+##### ContinualWorld environment
+'''
+ppo, baseline (no lifelong learning), task boundary (oracle) given
+'''
+def ppo_baseline_continualworld(name, args):
+    env_config_path = args.env_config_path
+
+    config = Config()
+    config.env_name = name
+    config.env_config_path = env_config_path
+    config.lr = 0.00015
+    config.cl_preservation = 'baseline'
+    config.seed = 8379
+    random_seed(config.seed)
+    exp_id = ''
+    log_name = name + '-ppo' + '-' + config.cl_preservation + exp_id
+    config.log_dir = get_default_log_dir(log_name)
+    config.num_workers = 4
+
+    # get num_tasks from env_config
+    with open(env_config_path, 'r') as f:
+        env_config_ = json.load(f)
+    num_tasks = len(env_config_['tasks'])
+    del env_config_
+
+    task_fn = lambda log_dir: ContinualWorld(name, env_config_path, log_dir, config.seed)
+    config.task_fn = lambda: ParallelizedTask(task_fn, config.num_workers, log_dir=config.log_dir)
+    eval_task_fn = lambda log_dir: ContinualWorld(name, env_config_path, log_dir, config.seed)
+    config.eval_task_fn = eval_task_fn
+    config.optimizer_fn = lambda params, lr: torch.optim.RMSprop(params, lr=lr)
+    config.network_fn = lambda state_dim, action_dim, label_dim: GaussianActorCriticNet_CL(
+        state_dim, action_dim, label_dim, 
+        phi_body=FCBody_CL(state_dim, task_label_dim=label_dim, hidden_units=(200, 200, 200)), 
+        actor_body=DummyBody_CL(200), 
+        critic_body=DummyBody_CL(200))
+    config.policy_fn = SamplePolicy
+    config.state_normalizer = RescaleNormalizer(1.) # no rescaling
+    config.discount = 0.99
+    config.use_gae = True
+    config.gae_tau = 0.99
+    config.entropy_weight = 0.01 #0.1 #0.75
+    config.rollout_length = 256 #128
+    config.optimization_epochs = 8
+    config.num_mini_batches = 64
+    config.ppo_ratio_clip = 0.1
+    config.iteration_log_interval = 1
+    config.gradient_clip = 5
+    config.max_steps = args.max_steps
+    config.evaluation_episodes = 1 #10
+    config.logger = get_logger(log_dir=config.log_dir, file_name='train-log')
+    config.cl_requires_task_label = True
+
+    config.eval_interval = 25
+    config.task_ids = np.arange(num_tasks).tolist()
+
+    agent = BaselineAgent(config)
+    config.agent_name = agent.__class__.__name__
+    tasks = agent.config.cl_tasks_info
+    config.cl_num_learn_blocks = 1
+    shutil.copy(env_config_path, config.log_dir + '/env_config.json')
+    with open('{0}/tasks_info.bin'.format(config.log_dir), 'wb') as f:
+        pickle.dump(tasks, f)
+    run_iterations_w_oracle(agent, tasks)
+    with open('{0}/tasks_info_after_train.bin'.format(config.log_dir), 'wb') as f:
+        pickle.dump(tasks, f)
+    # save config
+    with open('{0}/config.json'.format(config.log_dir), 'w') as f:
+        dict_config = vars(config)
+        for k in dict_config.keys():
+            if not isinstance(dict_config[k], int) \
+            and not isinstance(dict_config[k], float) and dict_config[k] is not None:
+                dict_config[k] = str(dict_config[k])
+        json.dump(dict_config, f)
+
 if __name__ == '__main__':
     mkdir('log')
     set_one_thread()
@@ -346,6 +420,14 @@ if __name__ == '__main__':
             ppo_baseline_mctgraph(name, args)
         elif args.algo == 'll_supermask':
             ppo_ll_mctgraph(name, args)
+        else:
+            raise ValueError('algo {0} not implemented'.format(args.algo))
+    elif args.env_name == 'continualworld':
+        name = 'ContinualWorld'
+        if args.algo == 'baseline':
+            ppo_baseline_continualworld(name, args)
+        elif args.algo == 'll_supermask':
+            ppo_ll_continualworld(name, args)
         else:
             raise ValueError('algo {0} not implemented'.format(args.algo))
     else:
