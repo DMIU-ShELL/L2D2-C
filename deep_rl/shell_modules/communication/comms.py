a936c2e7fc989bb0f11397ba6c3e9cec36a156c8
cosn5 <cosn5@lnx-grid-19.lunet.lboro.ac.uk>
2023-02-06 13:58:43 +0000

    logging unique connections

--- deep_rl/shell_modules/communication/comms.py
+++ deep_rl/shell_modules/communication/comms.py
@@ -105,7 +105,7 @@ class ParallelComm(object):
         self.handler = ipinfo.getHandler(access_token)
 
         details = self.handler.getDetails(self.init_address)
-        self.connections = [[details.hostname, details.ip, self.init_port, details.country, details.city, details.region, details.timezone, details.postal, details.latitude, details.longitude]]
+        self.connections = [[details.hostname, details.ip, self.init_port, details.country, details.city, details.region, details.timezone, details.postal, details.latitude, details.longitude, time.time()]]
             
 
         print(type(self.query_list))
@@ -302,7 +302,7 @@ class ParallelComm(object):
             
             
             details = self.handler.getDetails(ret['sender_address'])
-            self.connections.append([details.hostname, details.ip, ret['sender_port'], details.country, details.city, details.region, details.timezone, details.postal, details.latitude, details.longitude])
+            self.connections.append([details.hostname, details.ip, ret['sender_port'], details.country, details.city, details.region, details.timezone, details.postal, details.latitude, details.longitude, time.time()])
             np.savetxt(self.logger.log_dir + '/connections.csv', self.connections, delimiter=', ', fmt='%s')
 
         self.world_size.value = len(self.query_list) + 1    # Refresh the world_size value
